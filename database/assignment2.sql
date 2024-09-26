939ed8f26a0d74e3dfb82a09eee0662d7ac969db
Diego Vargas <diego.vargasd@gmail.com>
2024-09-25 19:10:33 -0600

    Created SQL database

---
+++ database/assignment2.sql
@@ -0,0 +1,13 @@
+-- Insert the following new record to the account table
+-- Tony, Stark, tony@starkent.com, Iam1ronM@n
+
+-- Modify the Tony Stark record to change the account_type to "Admin".
+
+-- Delete the Tony Stark record from the database.
+
+-- Modify the "GM Hummer" record to read "a huge interior" rather than "small interiors" using a single query.
+
+-- Use an inner join to select the make and model fields from the inventory table and the classification name field from the classification table for inventory items that belong to the "Sport" category.
+
+-- Update all records in the inventory table to add "/vehicles" to the middle of the file path in the inv_image and inv_thumbnail columns using a single query.
+
