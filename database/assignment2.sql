a1d8bcd74137345d7c4c7f998d2ac82809f0451a
Diego Vargas <diego.vargasd@gmail.com>
2024-09-27 14:52:15 -0600

    Added new queries to DB

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,13 +1,48 @@
+-- Check data
+SELECT *
+FROM account;
+-- change the name of the table
+-- Find Account ID
+SELECT account_id
+FROM account
+WHERE account_firstname = 'Tony'
+    AND account_lastname = 'Stark';
 -- Insert the following new record to the account table
 -- Tony, Stark, tony@starkent.com, Iam1ronM@n
-
+INSERT INTO account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
 -- Modify the Tony Stark record to change the account_type to "Admin".
-
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_id = 1;
 -- Delete the Tony Stark record from the database.
-
+DELETE FROM account
+WHERE account_id = 1;
 -- Modify the "GM Hummer" record to read "a huge interior" rather than "small interiors" using a single query.
-
+UPDATE inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'the small interiors',
+        'a huge interior'
+    ) --WHERE inv_make = 'GM' AND inv_model = 'Hummer'; it can be used instead of the the id
+WHERE inv_id = 10;
 -- Use an inner join to select the make and model fields from the inventory table and the classification name field from the classification table for inventory items that belong to the "Sport" category.
-
+SELECT i.inv_make,
+    i.inv_model
+FROM inventory i
+    INNER JOIN classification c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
 -- Update all records in the inventory table to add "/vehicles" to the middle of the file path in the inv_image and inv_thumbnail columns using a single query.
-
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
