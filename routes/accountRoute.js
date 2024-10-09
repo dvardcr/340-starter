5163be21d34eba43de514d6f8909e985f2946894
Diego Vargas <diego.vargasd@gmail.com>
2024-10-09 10:11:22 -0600

    Completed HTML Forms

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -7,4 +7,10 @@ const accountController = require("../controllers/accountController")
 // Route to display "Account" page
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
+// Route to display "Registration" page
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+// Route to POST "Registration" form
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
 module.exports = router;
